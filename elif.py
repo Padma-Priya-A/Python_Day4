552ba8e5adbc32815bbeb6263783a71618227dfd
Padma_Priya <116820600+Padma-Priya-A@users.noreply.github.com>
2023-10-01 15:01:47 +0530

    elif.py

--- elif.py
+++ elif.py
@@ -1,4 +1,4 @@
-#Get input for score out of 100.If score is < 35="Poor Student".if score is greater than 35 but < than 70 ="Average Student" if score is greater than 70="Good Student"
+#Get input for score out of 100.If score is < 35"Poor Student".if score is greater than 35 but < than 70 "Average Student" if score is greater than 70"Good Student"
 
 Score=int(input("Enter the score out of 100:"))
 if(Score<35):
